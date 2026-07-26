a3f18f277109af0a1decbdf7a085d71803d855e9
a35085605 <a35085605@gmail.com>
2026-07-26 22:48:09 +0800

    Expose template asset API

---
+++ template_assets/__init__.py
@@ -0,0 +1,24 @@
+from template_assets.domain.locators import (
+    FileLocator,
+    HttpLocator,
+    PackageLocator,
+    TemplateLocator,
+)
+from template_assets.domain.manifest import (
+    TemplateManifestEntry,
+    TemplateProvenance,
+    TemplateStorageDefinition,
+)
+from template_assets.domain.models import GrayImage, Template
+
+__all__ = [
+    "FileLocator",
+    "GrayImage",
+    "HttpLocator",
+    "PackageLocator",
+    "Template",
+    "TemplateLocator",
+    "TemplateManifestEntry",
+    "TemplateProvenance",
+    "TemplateStorageDefinition",
+]
