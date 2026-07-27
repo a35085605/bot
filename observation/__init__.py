006b365cfe664cffda419f27407f76bcf15d8772
a35085605 <a35085605@gmail.com>
2026-07-27 20:59:00 +0800

    Add observation contract exports

---
+++ observation/__init__.py
@@ -0,0 +1,27 @@
+from observation.domain.models import (
+    CapturedFrame,
+    CaptureQuality,
+    CaptureStreamId,
+    CoordinateSpace,
+    CoordinateTransform,
+    FrameId,
+    FrameInfo,
+    FramePixels,
+    PixelFormat,
+    WindowContext,
+)
+from observation.ports import FrameSource
+
+__all__ = [
+    "CapturedFrame",
+    "CaptureQuality",
+    "CaptureStreamId",
+    "CoordinateSpace",
+    "CoordinateTransform",
+    "FrameId",
+    "FrameInfo",
+    "FramePixels",
+    "FrameSource",
+    "PixelFormat",
+    "WindowContext",
+]
