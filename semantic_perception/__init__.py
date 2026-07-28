545ef0d69d0d9faf551cdabeda1aacbd71c2aaa5
a35085605 <a35085605@gmail.com>
2026-07-28 11:47:18 +0800

    Add semantic perception package exports

---
+++ semantic_perception/__init__.py
@@ -0,0 +1,15 @@
+from semantic_perception.rules import (
+    ControlRule,
+    EvidenceRequirement,
+    SceneRule,
+    SemanticPerceptionConfig,
+)
+from semantic_perception.snapshot_builder import SemanticSnapshotBuilder
+
+__all__ = [
+    "ControlRule",
+    "EvidenceRequirement",
+    "SceneRule",
+    "SemanticPerceptionConfig",
+    "SemanticSnapshotBuilder",
+]
