da68fdba9395ccb573f03f117030693ca3128279
a35085605 <a35085605@gmail.com>
2026-07-29 18:49:35 +0800

    Introduce capture boundary models

---
+++ capture/__init__.py
@@ -0,0 +1,27 @@
+from capture.domain.models import (
+    CapturedFrame,
+    CaptureQuality,
+    CaptureStreamId,
+    CaptureSurface,
+    CoordinateSpace,
+    CoordinateTransform,
+    FrameId,
+    FrameInfo,
+    FramePixels,
+    PixelFormat,
+)
+from capture.ports import FrameSource
+
+__all__ = [
+    "CapturedFrame",
+    "CaptureQuality",
+    "CaptureStreamId",
+    "CaptureSurface",
+    "CoordinateSpace",
+    "CoordinateTransform",
+    "FrameId",
+    "FrameInfo",
+    "FramePixels",
+    "FrameSource",
+    "PixelFormat",
+]
