2d734ed8e7b2559148d0b74fcdca50712cc3e74a
a35085605 <a35085605@gmail.com>
2026-07-31 02:36:02 +0800

    Keep target runtime domain compatibility

--- target_runtime/domain/__init__.py
+++ target_runtime/domain/__init__.py
@@ -1,35 +1,4 @@
-from target_runtime.domain.channels import (
-    AdbChannelState,
-    ControlChannelSnapshot,
-    WindowChannelState,
-)
-from target_runtime.domain.identities import (
-    ControlChannelId,
-    ReadinessBlocker,
-    TargetId,
-)
-from target_runtime.domain.readiness import (
-    AdbDeviceStatus,
-    ControlCapability,
-    ControlChannelKind,
-    ControlChannelStatus,
-    FocusStatus,
-    TargetAvailability,
-)
-from target_runtime.domain.snapshots import TargetRuntimeSnapshot
+"""Deprecated compatibility facade for ``observation.target_runtime.domain``."""
 
-__all__ = [
-    "AdbChannelState",
-    "AdbDeviceStatus",
-    "ControlCapability",
-    "ControlChannelId",
-    "ControlChannelKind",
-    "ControlChannelSnapshot",
-    "ControlChannelStatus",
-    "FocusStatus",
-    "ReadinessBlocker",
-    "TargetAvailability",
-    "TargetId",
-    "TargetRuntimeSnapshot",
-    "WindowChannelState",
-]
+from observation.target_runtime.domain import *
+from observation.target_runtime.domain import __all__
