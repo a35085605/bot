a9b4dff24d5e691aa315638112ea60e8f59df36f
a35085605 <a35085605@gmail.com>
2026-08-01 20:11:12 +0800

    Remove deprecated compatibility facades

--- target_runtime/domain/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Deprecated compatibility facade for ``observation.target_runtime.domain``."""
-
-from observation.target_runtime.domain import *
-from observation.target_runtime.domain import __all__
