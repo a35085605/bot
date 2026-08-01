4f90249505dc066047f06a8949b46259c38aec00
a35085605 <a35085605@gmail.com>
2026-08-01 20:03:56 +0800

    Migrate compatibility imports

--- world_model/tracker.py
+++ world_model/tracker.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta
 from numbers import Integral
 
-from capture import FrameId
+from observation.capture import FrameId
 from world_model.identities import (
     ControlKey,
     IndicatorKey,
