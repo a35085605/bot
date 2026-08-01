bdca2ac33f248f1b6545109f10c43e777625c8e3
a35085605 <a35085605@gmail.com>
2026-08-01 23:58:12 +0800

    Export visual target binding contracts

---
+++ visual_target_binding/__init__.py
@@ -0,0 +1,17 @@
+from visual_target_binding.domain import (
+    VisualTargetBinding,
+    VisualTargetBindingBasis,
+    VisualTargetBindingFailureReason,
+    VisualTargetBindingResult,
+    VisualTargetBindingUnavailable,
+)
+from visual_target_binding.ports import VisualTargetBinder
+
+__all__ = [
+    "VisualTargetBinder",
+    "VisualTargetBinding",
+    "VisualTargetBindingBasis",
+    "VisualTargetBindingFailureReason",
+    "VisualTargetBindingResult",
+    "VisualTargetBindingUnavailable",
+]
