7d97f5fc1c19aed7b80c42de8b04dd0516f16d3d
a35085605 <a35085605@gmail.com>
2026-08-06 12:47:58 +0800

    Add desktop window observation package

---
+++ desktop_window/__init__.py
@@ -0,0 +1,6 @@
+"""Desktop-window interaction contracts.
+
+Platform-specific observation contracts are exposed from
+``desktop_window.observation``. Additional capability families may move under
+this vertical package in later migration stages.
+"""
