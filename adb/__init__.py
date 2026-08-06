8487d52b1422a864dd577a95785d50416ee416d8
a35085605 <a35085605@gmail.com>
2026-08-06 12:48:50 +0800

    Add ADB observation package

---
+++ adb/__init__.py
@@ -0,0 +1,6 @@
+"""ADB interaction contracts.
+
+Platform-specific observation contracts are exposed from ``adb.observation``.
+Additional capability families may move under this vertical package in later
+migration stages.
+"""
